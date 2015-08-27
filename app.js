a51829e420f0aea5d3bdd97f06e9190ddfacf298
manureta <manuel.retamozo@gmail.com>
2015-08-27 17:31:17 -0300

    Fix busqEUda

--- app.js
+++ app.js
@@ -378,7 +378,7 @@ xtype: "combo",
 
 // BUSQUEDAS
 	busqueda_barrio = new Busqueda_Barrio(this);
-	busqueda_folio = new Busqeuda_Folio(this);
+	busqueda_folio = new Busqueda_Folio(this);
 	
     //add highlight of identified object to map, called from feature-info render-event, configured in userconfig.ashx (from database))
     function addHighlight(feature) {
